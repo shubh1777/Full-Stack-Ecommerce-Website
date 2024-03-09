74c3f641a14bc42bd4ff78be140e594298dff444
shubh1777 <114337784+shubh1777@users.noreply.github.com>
2024-03-09 18:02:46 +0530

    setup react/tailwind and initial pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-     
+    <Home></Home>
     </div>
   );
 }
